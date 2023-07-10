c5b36c29286300a398454859dc9e87ef1aa6b9cb
Ícaro Cassiolato <icaro.a.m.cassiolato@gmail.com>
2023-07-10 17:14:49 -0300

    do zero de novo

--- Classes/Resultado.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NonFollowersAPI.Classes
-{
-    public class Resultado
-    {
-        public int Seguindo { get; set; }
-        public int Seguidores { get; set; }
-        public List<ExternalUser> NaoSeguidores { get; set; }
-    }
-}
