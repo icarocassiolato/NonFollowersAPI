3aa6da93c3cbce92450d79e7eb723323ede000a1
Ícaro Cassiolato <icaro.a.m.cassiolato@gmail.com>
2023-07-10 15:13:59 -0300

    adicionando swagger

--- Program.cs
+++ Program.cs
@@ -1,16 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseHttpsRedirection()
+    .UseAuthentication()
+    .UseAuthorization()
+    .UseCors("AllowAllHeaders");
 
 app.MapControllers();
 
